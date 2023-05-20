e1e91894bc3b735a72a5637c429e4b9e68f6eaa1
rcshpark <rcshpark@gmail.com>
2023-05-20 19:19:39 +0900

    capstone

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ChatbotPage from "./pages/chatbot";
 import Login from "./pages/login";
+import Main from "./pages/main";
 function App() {
   return (
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<ChatbotPage/>}/>
     <Route path="/login" element={<Login/>}/>
+    <Route path="/main" element={<Main/>}/>
   </Routes>
   </BrowserRouter>
   );
